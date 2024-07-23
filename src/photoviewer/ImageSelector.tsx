b52e3d0b98c98736e7b8f38fd10f5d101ede9053
Purbai <purbai.varsani@softwire.com>
2024-07-23 13:56:32 +0100

    added interface props

--- src/photoviewer/ImageSelector.tsx
+++ src/photoviewer/ImageSelector.tsx
@@ -4,18 +4,23 @@ import {imageUrls} from './GetImageUrls';
 import "./PhotoViewer.css";
 // import {imageSelectedUrl, setImageSelectedUrl} from '../App'
 
+interface SelectImageProps {
+    imageSelectedUrl: string;
+    setImageSelectedUrl: (url: string) => void;
+  }
 
-export function ImageSelector( {imageSelectedUrl}:{imageSelectedUrl:string} , {setImageSelectedUrl} : {setImageSelectedUrl:(url:string) => void}) : JSX.Element {// declare and export new function called 'ImageSelector'
+
+export function ImageSelector( props : SelectImageProps) : JSX.Element {// declare and export new function called 'ImageSelector'
     // const [imageselectedUrl, setImageSelectedUrl] = useState("");
     const thumbNails = imageUrls.map(
-        (url) => 
-        <img className={(imageSelectedUrl === url ? "ImageSelected": "ImageNotSelected")} 
-        onClick={() => {setImageSelectedUrl(url)}} src={url} />)
+        (url) => <img className={(props.imageSelectedUrl === url ? "ImageSelected": "ImageNotSelected")} 
+        onClick={() => {props.setImageSelectedUrl(url)}} src={url} />)
 
     return (                
         <div >               
             {/* display the thumbnail images  */}
             {thumbNails}
+
         </div>
     );
 }
